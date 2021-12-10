9efeb4c91010e0a1815f27cf48ce57be06d921b9
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2021-12-10 21:06:03 +0300

    Урок 5. Операторы, фильтрация, сортировка и ограничение. Агрегация данных

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Contains:
   - [Урок 2. Управление БД. Язык запросов SQL](https://github.com/AlekseyGur/Education/tree/master/SQL/lesson_2)
   - [Урок 3. Введение в проектирование БД](https://github.com/AlekseyGur/Education/tree/master/SQL/lesson_3)
   - [Урок 4. CRUD-операции](https://github.com/AlekseyGur/Education/tree/master/SQL/lesson_4)
+  - [Урок 5. Операторы, фильтрация, сортировка и ограничение. Агрегация данных](https://github.com/AlekseyGur/Education/tree/master/SQL/lesson_5)
 - [HTML/CSS](https://github.com/AlekseyGur/Education/tree/master/HTML-CSS)
   - [Урок 2. Основы языка разметки документов HTML](https://github.com/AlekseyGur/Education/tree/master/HTML-CSS/lesson_2)
   - [Урок 3. Основы языка оформления стилей документа CSS](https://github.com/AlekseyGur/Education/tree/master/HTML-CSS/lesson_3)
---
+++ SQL/lesson_5/task.sql
@@ -0,0 +1,50 @@
+/* 
+Практическое задание по теме “Операторы, фильтрация,  сортировка и ограничение” 
+*/
+
+-- Задание 1
+-- заполним created_at и updated_at текущей датой и временем
+UPDATE users SET created_at = NOW(), updated_at = NOW();
+
+
+-- Задание 2
+-- преобразуем данные в нужный формат типа
+UPDATE users SET created_at = STR_TO_DATE(created_at, '%d.%m.%Y %h:%i'), updated_at = STR_TO_DATE(updated_at, '%d.%m.%Y %h:%i');
+-- изменяем тип столбцов
+ALTER TABLE users MODIFY created_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE users MODIFY updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+
+-- Задание 3
+-- вывести складские запасы по возрастаню запасов, но отсутствующие должны быть в конце списка
+SELECT *, CASE WHEN value = 0 THEN 0 ELSE 1 END AS exist FROM storehouses_products ORDER BY exist DESC, value;
+
+
+-- Задание 4
+-- из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. Месяцы заданы в виде списка английских названий ('may', 'august')
+SELECT *, MONTHNAME(birthday_at) AS month FROM users WHERE MONTHNAME(birthday_at) RLIKE 'may|august';
+
+
+-- Задание 5
+-- Отсортируйте записи в порядке, заданном в списке IN.
+SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(id,5,1,2);
+
+
+/* 
+Практическое задание теме “Агрегация данных” 
+*/
+
+-- Задание 1
+-- средний возраст пользователей в таблице users
+SELECT round(AVG(TIMESTAMPDIFF(YEAR, birthday_at, NOW()))) AS AVG_AGE FROM users;
+
+
+-- Задание 2
+-- количество дней рождения, которые приходятся на каждый из дней недели. Необходимы дни недели текущего года, а не года рождения.
+SELECT DAYOFWEEK( DATE_FORMAT( birthday_at, CONCAT(YEAR(NOW()), '-%m-%d') ) ) AS day, COUNT(*) AS cnt FROM use
+rs GROUP BY day; -- (1 = воскресенье, …, 7 = суббота)
+
+
+-- Задание 3
+-- произведение чисел в столбце таблицы
+SELECT ROUND(EXP(SUM(LN(id)))) AS 'RESULT' FROM numbers;
